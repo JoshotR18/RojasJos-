63e3ed23e8ac9249c9315707793beee2a92dd962
JoshotR18 <137571571+JoshotR18@users.noreply.github.com>
2024-06-25 20:04:47 -0400

    Add files via upload

---
+++ Gestudiantes/src/main/java/com/mycompany/gestudiantes/Gestudiantes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestudiantes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Gestudiantes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
